4c7f57f66c6c6f44ae4a11550dd3e03e58d03c3f
Iran Adryan <contatoiranadryan@gmail.com>
2020-04-16 17:01:30 -0300

    Atualizando as rotas

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import Login from './pages/Login';
-import Reset from './pages/Reset';
-import Register from './pages/Register';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Reset from './pages/Reset';
 import ChangePassword from './pages/ChangePassword';
 
 export default function Routes() {
@@ -17,8 +17,8 @@ export default function Routes() {
           {logged ? <Home /> : <Redirect to="/login" />}
         </Route>
         <Route path="/login" component={Login} />
-        <Route path="/reset" component={Reset} />
         <Route path="/register" component={Register} />
+        <Route path="/reset" component={Reset} />
         <Route path="/change-password" component={ChangePassword} />
       </Switch>
     </BrowserRouter>
